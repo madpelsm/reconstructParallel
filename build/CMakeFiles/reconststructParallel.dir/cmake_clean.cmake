a431216df0075ecc8e438ef3596c9eb6e103e47c
madpelsm <mathias.depelsmaeker@ugent.be>
2019-02-16 14:31:00 +0100

    first commit

---
+++ build/CMakeFiles/reconststructParallel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reconststructParallel.dir/src/main.cpp.o"
+  "reconststructParallel.pdb"
+  "reconststructParallel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reconststructParallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
